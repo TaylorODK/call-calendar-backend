97b6dc8f01d7e8a48b4caf15920b55388fb0d404
Mikhail Bashirov <bashirovma@gmail.com>
2025-12-16 22:17:40 +0800

    first commit

--- backend/calendar_backend/urls.py
+++ backend/calendar_backend/urls.py
@@ -17,9 +17,12 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("users.urls")),
     path("api/", include("event.urls")),
+    path("healthcheck/", lambda request: HttpResponse("OK")),
 ]
