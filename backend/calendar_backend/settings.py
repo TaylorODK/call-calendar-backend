3fecd6188d12dcd399fe04fdce955256c9cacf81
Ильин Александр Леонидович <ilin.al@esphere.ru>
2025-12-16 20:41:18 +0300

    edit config

--- backend/calendar_backend/settings.py
+++ backend/calendar_backend/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
