7a6e4e0b794907995d2f016cfe348f78a695f772
Mikhail Bashirov <bashirovma@gmail.com>
2025-12-16 22:53:14 +0800

    review fix

--- backend/calendar_backend/urls.py
+++ backend/calendar_backend/urls.py
@@ -17,12 +17,9 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from django.http import HttpResponse
-
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("users.urls")),
     path("api/", include("event.urls")),
-    path("healthcheck/", lambda request: HttpResponse("OK")),
 ]
