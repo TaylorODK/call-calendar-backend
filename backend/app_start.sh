a6a5b64282cf42bdf963e2a4554eb130952892ab
Якомазов Алексей <a.yakomazov@ylab.team>
2025-12-17 10:07:18 +0000

    [fix] Добавлены доверенные CORS и CSRF, изменён порядок команд в app_start.sh [QNUL-144] [prod]

--- backend/app_start.sh
+++ backend/app_start.sh
@@ -3,7 +3,7 @@ set -e
 
 python backend/manage.py makemigrations users
 python backend/manage.py makemigrations event
+python backend/manage.py migrate
 python backend/manage.py migrate django_celery_beat
 python backend/manage.py collectstatic --noinput
-python backend/manage.py migrate
 python backend/manage.py runserver 0.0.0.0:8000
