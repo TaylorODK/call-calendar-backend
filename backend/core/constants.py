209c8dd7104c6e7bee6246a5eb3b1f589b22a90b
Mikhail Bashirov <bashirovma@gmail.com>
2026-03-01 05:58:46 +0300

    change constants.py for env source

--- backend/core/constants.py
+++ backend/core/constants.py
@@ -1,3 +1,4 @@
+import os
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -6,12 +7,11 @@ NAME_MAX_LENGTH = 255
 CODE_MAX_LENGTH = 4
 CODE_EXPIRATION_TIME = 1
 DATE_FORMAT_FOR_ALERTS = "%H:%M %d.%m.%Y"
-ALLOWED_EMAIL = ("@ylab.team", "@ylab.io")
-CALENDAR_KEY = "b2e59b301592d6a16043453b8257fd6e5a1cff89&tz_id=Europe/Moscow"
+ALLOWED_EMAIL = os.getenv("ALLOWED_EMAIL", "mail.ru").split(",")
+CALENDAR_KEY = os.getenv("CALENDAR_KEY")
 ALERT_TIME_BEFORE_EVENT = 10
 PARSIN_AHEAD_DAYS = 5
-# CALENDAR_KEY = os.getenv("CALENDAR_KEY")
-CHAT_ID = "-1003577506308"
+CHAT_ID = os.getenv("CHAT_ID")
 PARSE_URL = "https://calendar.yandex.ru/export/ics.xml?private_token="
 BYDAY_MAP = {
     0: "MO",
